f55fad9dce2d7cd606dbcee06db70ec833c1668a
Manitra <94074919+ShibaMiyuki07@users.noreply.github.com>
2025-03-23 20:22:47 +0300

    add method cors policy

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+function setupCORS(req, res, next) {
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-type,Accept,X-Access-Token,X-Key');
+    res.header('Access-Control-Allow-Origin', '*');
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+    } else {
+        next();
+    }
+}
+app.use(setupCORS);
+app.all('/*', setupCORS);
 // Routes
 app.use('/users', authMiddleware, userRoutes);
 app.use('/reservations', reservationRoutes);
